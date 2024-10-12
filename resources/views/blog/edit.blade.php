7cb0a70ceb35095dd1727466f20bbc496bf124cf
wisegear <lee@wisener.net>
2024-10-12 21:54:58 +0100

    tweaks

--- resources/views/blog/edit.blade.php
+++ resources/views/blog/edit.blade.php
@@ -58,8 +58,20 @@
             <textarea class="w-full border rounded" name="body" id="editor">{{ old('body', $post->body) }}</textarea>    
         </div>
 
-        <!-- Include TinyMCE from the npm installation -->
-        <script src="https://cdn.tiny.cloud/1/a1rn9rzvnlulpzdgoe14w7kqi1qpfsx7cx9am2kbgg226dqz/tinymce/6/tinymce.min.js" referrerpolicy="origin"></script>
+    <!-- Place the first <script> tag in your HTML's <head> -->
+    <script src="https://cdn.tiny.cloud/1/a1rn9rzvnlulpzdgoe14w7kqi1qpfsx7cx9am2kbgg226dqz/tinymce/7/tinymce.min.js" referrerpolicy="origin"></script>
+
+    <!-- Place the following <script> and <textarea> tags your HTML's <body> -->
+    <script>
+      tinymce.init({
+        selector: 'textarea',
+        plugins: 'anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount',
+        toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table | align lineheight | numlist bullist indent outdent | emoticons charmap | removeformat',
+      });
+    </script>
+    <textarea>
+      Welcome to TinyMCE!
+    </textarea>
 
         <script>
             tinymce.init({
