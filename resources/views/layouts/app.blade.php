2d0a7b596aa0f7aa4c4979a35c211ae973309160
wisegear <lee@wisener.net>
2024-10-13 21:20:12 +0100

    Favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,9 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
+        <!-- Favicon -->
+        <link rel="icon" type="image/svg+xml" href="{{ asset('/assets/images/site/house.svg') }}">
+
         <title>{{ config('app.name', 'TheHolocaust') }}</title>
 
         <!-- FontAwesome -->
