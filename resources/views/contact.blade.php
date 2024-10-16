ffab2fc985ad8510c2674e43f3fef6878c3f414d
wisegear <lee@wisener.net>
2024-10-16 20:51:29 +0100

    Add Support

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -11,6 +11,7 @@
         <p class="wise1text mb-2">
             The contact form I used got spammed so much that I removed it. For now, use the details below to get in touch. I am seeking a new way to control 
             spam and return the form. Visitors did use it a lot, but unfortunately, I kept missing genuine contact emails.</p>
+        <p class="font-bold mb-2">I have created a new support system, just login to use it and get in touch.  I would prefer you use this rather than email. </p>
         <p>Email: leejwisener@gmail.com</p>
         <p>Twitter/X: <a href="https://twitter.com/wisenerl">@wisenerl</a></p>
          
