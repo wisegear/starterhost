5ff0cea836fb7ae45cb2130055f33eeaa2a2536f
wisegear <lee@wisener.net>
2024-10-17 20:07:40 +0100

    Check Backups

--- routes/console.php
+++ routes/console.php
@@ -2,6 +2,15 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
+
+Schedule::command('backup:clean')->daily()->at('01:00');
+Schedule::command('backup:run')->daily()->at('01:30');
+// Run the clean command every minute
+Schedule::command('backup:clean')->everyMinute();
+
+// Run the backup command every minute
+Schedule::command('backup:run')->everyMinute();
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
