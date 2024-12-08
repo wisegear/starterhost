d528459e15001afc2047398e81b99681edabc466
Lee Wisener <leewisener@Lees-Mac-mini.local>
2024-12-08 12:56:36 +0000

    Added reCAPTCHA v3 to registration form

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -39,6 +39,16 @@
             <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
         </div>
 
+        <!-- HCatchpa -->
+
+        <div class="form-group my-6 flex justify-center">
+        {!! NoCaptcha::renderJs() !!}
+        {!! NoCaptcha::display() !!}
+        </div>
+        @if ($errors->has('g-recaptcha-response'))
+            <span class="text-danger">{{ $errors->first('g-recaptcha-response') }}</span>
+        @endif
+
         <div class="flex items-center justify-end mt-4">
             <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('login') }}">
                 {{ __('Already registered?') }}
