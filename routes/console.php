0eb2361c8995a01e39a4886b16dd89dd9bc23de6
Lee Wisener <leewisener@Lees-Mac-mini.local>
2025-01-09 20:03:05 +0000

    cron update

--- routes/console.php
+++ routes/console.php
@@ -5,8 +5,8 @@ use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
 
 Schedule::command('backup:clean')->daily()->at('01:00');
-Schedule::command('backup:run')->daily()->at('01:30');
+Schedule::command('backup:run')->daily()->at('01:00');
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote')->hourly();
+//Artisan::command('inspire', function () {
+//    $this->comment(Inspiring::quote());
+//})->purpose('Display an inspiring quote')->hourly();
